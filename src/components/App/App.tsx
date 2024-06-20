a6bc3bf37dbbf95220b74a2c2013b009e72e5184
Valentina Danilova <danilova.vv@phystech.edu>
2024-06-20 14:37:16 +0400

    feat: add marquee section

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,6 +2,7 @@ import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import AboutUs from '../AboutUs/AboutUs';
 import './App.styles.scss';
+import Marquee from '../Marquee/Marquee';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className='app-content'>
         <Header className='app__header' />
         <AboutUs className='app__about-us' />
+        <Marquee className='app__marquee' />
         <Footer />
       </div>
     </div>
---
+++ src/components/Marquee/Marquee.tsx
@@ -0,0 +1,61 @@
+import { gsap } from 'gsap';
+import './Marquee.styles.scss';
+import AmazonLogo from '../../../public/assets/amazon.svg?react';
+import DribbbleLogo from '../../../public/assets/dribbble.svg?react';
+import HubspotLogo from '../../../public/assets/hubspot.svg?react';
+import NotionLogo from '../../../public/assets/notion.svg?react';
+import NetflixLogo from '../../../public/assets/netflix.svg?react';
+import ZoomLogo from '../../../public/assets/zoom.svg?react';
+import { useRef, useLayoutEffect, useEffect } from 'react';
+
+function Marquee({ className }: { className?: string }) {
+  const innerRef = useRef<HTMLDivElement>(null);
+  const marqueePart = (
+    <div className='marquee__part'>
+      <AmazonLogo />
+      <DribbbleLogo />
+      <HubspotLogo />
+      <NotionLogo />
+      <NetflixLogo />
+      <ZoomLogo />
+    </div>
+  );
+  const timeline = gsap.timeline({ repeat: -1 });
+
+  useLayoutEffect(() => {
+    timeline
+      .to(innerRef.current, { duration: 20, xPercent: -50, ease: 'none' })
+      .set(innerRef.current, { xPercent: 0 });
+  }, []);
+
+  useEffect(() => {
+    const marquee = innerRef.current;
+    if (marquee) {
+      const pauseMarquee = () => {
+        timeline.pause();
+      };
+      const playMarquee = () => {
+        timeline.play();
+      };
+
+      marquee.addEventListener('mouseenter', pauseMarquee);
+      marquee.addEventListener('mouseleave', playMarquee);
+
+      return () => {
+        marquee.removeEventListener('mouseenter', pauseMarquee);
+        marquee.removeEventListener('mouseleave', playMarquee);
+      };
+    }
+  }, []);
+
+  return (
+    <section id='marquee' className={`marquee ${className || ''}`}>
+      <div className='marquee__inner' ref={innerRef}>
+        {marqueePart}
+        {marqueePart}
+      </div>
+    </section>
+  );
+}
+
+export default Marquee;
